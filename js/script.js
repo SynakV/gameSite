694b19c75ad6d0616bf93b59085af29b5b1e71e8
SynakV <36859144+SynakV@users.noreply.github.com>
2018-08-16 01:48:53 +0300

    Add files via upload
    added slider

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
-    $(".headerMenuLinks").on("click","a", function (event) {
+    
+    $(".headerMenuLinks").on("click","li", function (event) {
         event.preventDefault();
         var id  = $(this).attr('href'),
-            top = $(id).offset().top;
+            top = $(id).offset().top-60;
         $('body,html').animate({scrollTop: top}, 1000);
     });
+    
 });
