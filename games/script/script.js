aea3fff261dae90ade914189a13ae44104e6b53e
Vasyl Sunak <sunack16@gmail.com>
2018-08-21 01:09:15 +0300

    added games

--- games/script/script.js
+++ games/script/script.js
@@ -1,5 +1,54 @@
 $(document).ready(function() {
+    
+    if ($("body").attr("id") == "gothic3") {
+        var gothic3Img = ['1.jpg', '2.jpg', '3.jpg'];
+        var gothic3Vid = ['https://www.youtube.com/embed/ixzK0jqLO70', 'https://www.youtube.com/embed/oxoFVqetl1E', 'https://www.youtube.com/embed/l0QGLMwR-lY'];
+
+        for ( var i = 0; i < gothic3Vid.length; i++ ) {
+            $(".sliderContent").append("<iframe src=" + gothic3Vid[i] + " frameborder=0 allowfullscreen" + "></iframe>");
+        }
+
+        for ( var i = 0; i < gothic3Img.length; i++ ) {
+            $(".sliderContent").append("<img src=" + "img/" + gothic3Img[i] + ">");
+            $(".sliderContent > *").css("display", "none");
+            $(".sliderContent > *:nth-child(1)").css("display", "block");
+        }
+    }
+    
+    if ($("body").attr("id") == "nfsmw") {
+        var nfsmwImg = ['1.jpg', '2.jpg', '3.jpg'];
+        var nfsmwVid = ['https://www.youtube.com/embed/ixzK0jqLO70', 'https://www.youtube.com/embed/oxoFVqetl1E', 'https://www.youtube.com/embed/l0QGLMwR-lY'];
+
+        for ( var i = 0; i < nfsmwVid.length; i++ ) {
+            $(".sliderContent").append("<iframe src=" + nfsmwVid[i] + " frameborder=0 allowfullscreen" + "></iframe>");
+        }
+
+        for ( var i = 0; i < nfsmwImg.length; i++ ) {
+            $(".sliderContent").append("<img src=" + "img/" + nfsmwImg[i] + ">");
+            $(".sliderContent > *").css("display", "none");
+            $(".sliderContent > *:nth-child(1)").css("display", "block");
+        }
+    }
+    
+    var sliderContentLength = $(".sliderContent > *");
+    
+    for ( var i = 0; i < sliderContentLength.length; i++ ) {
+        $(".sliderBull").append("<li>&bull;</li>");
+    }
+    
+    var bulls = $(".sliderBull > *");
+    
+    for ( let i = 0; i < bulls.length; i++ ) {
+        $(bulls[i]).click(function() {
+            $(sliderContentLength).each(function() {
+                $(this).css("display", "none");
+            })
+            $(sliderContentLength[i]).fadeIn().css("display", "block");
+        })
+    }
+    
     $(".backButton").click(function() {
         history.back(-1);
     })
+    
 })
--- js/slider.js
+++ js/slider.js
@@ -1,24 +1,32 @@
 $(document).ready(function(){
     
     var sliderImgArr = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg'];
-    var sliderVidArr = ['https://www.youtube.com/embed/ixzK0jqLO70'];
+    var sliderVidArr = ['https://www.youtube.com/embed/ixzK0jqLO70', 'https://www.youtube.com/embed/oxoFVqetl1E', 'https://www.youtube.com/embed/l0QGLMwR-lY'];
+    
+    for ( var i = 0; i < sliderVidArr.length; i++ ) {
+        $(".sliderContent").append("<iframe src=" + sliderVidArr[i] + " frameborder=0 allowfullscreen" + "></iframe>");
+    }
     
     for ( var i = 0; i < sliderImgArr.length; i++ ) {
         $(".sliderContent").append("<img src=" + "img/sliderImg/" + sliderImgArr[i] + ">");
+        $(".sliderContent > *").css("display", "none");
+        $(".sliderContent > *:nth-child(1)").css("display", "block");
+    }
+    
+    var sliderContentLength = $(".sliderContent > *");
+    
+    for ( var i = 0; i < sliderContentLength.length; i++ ) {
         $(".sliderBull").append("<li>&bull;</li>");
-        $(".slider img").css("display", "none");
-        $(".slider img:nth-child(1)").css("display", "block");
     }
     
     var bulls = $(".sliderBull > *");
-    var sliderContentImg = $(".sliderContent > *");
     
     for ( let i = 0; i < bulls.length; i++ ) {
         $(bulls[i]).click(function() {
-            $(sliderContentImg).each(function() {
+            $(sliderContentLength).each(function() {
                 $(this).css("display", "none");
             })
-            $(sliderContentImg[i]).fadeIn().css("display", "block");
+            $(sliderContentLength[i]).fadeIn().css("display", "block");
         })
     }
     
